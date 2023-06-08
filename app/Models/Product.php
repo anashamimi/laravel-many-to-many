f33d8ea7f7af3c7bfdd390077830c3bd5be94572
anashamimi <anashamimi2@gmail.com>
2023-06-08 21:43:17 +0200

    add relation many-to-many with types table

---
+++ app/Models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable =['title', 'price' , 'description', 'image'];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+}
